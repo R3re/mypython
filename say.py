f7068c25010e4b28cc2616d7b011cd4adbe14b50
R3re <vspbgo@gmail.com>
2018-05-22 13:19:43 +0500

    Приветствие

--- say.py
+++ say.py
@@ -1,3 +1,5 @@
+#Приветствие
+
 def say_hi(name, age):
 
     return "Hi. My name is {} and I'm {} years old".format(name, age)
@@ -5,5 +7,3 @@ def say_hi(name, age):
 if __name__=='__main__':
     say_hi("Dima", 26)
     print(say_hi("Дима", 26))
-
-#Проверка коммита
