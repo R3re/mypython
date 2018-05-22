72d423949747019da422777b4af7c43b84bf1cfe
R3re <36309162+R3re@users.noreply.github.com>
2018-05-22 12:06:56 +0500

    Первые задачи
    Начал изучать Python

---
+++ import_example.py
@@ -0,0 +1,4 @@
+import say
+
+if __name__ == '__main__':
+    print(say.say_hi("Даша", 20))
---
+++ say.py
@@ -0,0 +1,7 @@
+def say_hi(name, age):
+
+    return "Hi. My name is {} and I'm {} years old".format(name, age)
+
+if __name__=='__main__':
+    say_hi("Dima", 26)
+    print(say_hi("Дима", 26))
